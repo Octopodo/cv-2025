f671f017e3a5f8a10ce4e764a450d65e3b5121b6
Octopodo <orson_wells40@hotmail.com>
2025-03-27 18:49:53 +0100

    Changed base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Octopodo.github.io',
-    base: '/cv-fullstack',
+    base: '/cv-2025',
     output: 'static'
 });
